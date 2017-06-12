23c2f0e3932ae5f1bccb2dbda6879b7951cb8eeb
Nathaniel Jones <n8jhj@live.com>
2017-06-12 16:29:36 -0700

    partitionByDays.m is finished
    partitionByDays.m takes in timestamped data and returns it as a list of
    structures containing the same data, only partitioned by days. Also
    returns which days have no data.

--- MATLAB/partitionByDays.m
+++ MATLAB/partitionByDays.m
@@ -1,10 +1,63 @@
-function days = partitionByDays(data)
-%PARTITIONBYDAYS Get input data returned as cell array of days of data.
-%   days = partitionByDays(data) Partitions input struct DATA into days
-%   using its property TIMESTAMP.
+function [days, emptyDays] = partitionByDays(data)
+%PARTITIONBYDAYS Get input data returned as structure array of the data
+%partitioned by days.
+%   [days, emptyDays] = partitionByDays(data) Partitions input struct DATA
+%   into days using its property TIMESTAMP. DAYS is the partitioned
+%   structure array and EMPTYDAYS is an array of datetime values
+%   corresponding to the days for which there was no data.
 
-%% Preallocate size of cell array to return.
+%% Get fieldnames of data
+fdNames = fieldnames(data);
+f1 = char(fdNames(1));
+f2 = char(fdNames(2));
+f3 = char(fdNames(3));
+f4 = char(fdNames(4));
+f5 = char(fdNames(5));
+
+%% Check input for field timestamp as first field
+assert(strcmp(f1, 'timestamp'), ...
+    'Input struct DATA must have TIMESTAMP as first field')
+
+%% Preallocate size of cell array to return
 tnum = datenum(data.timestamp);
-nDays = tnum(end) - tnum(1);
+nDays = ceil(tnum(end)) - floor(tnum(1));
+days = struct(f1,{}, f2,{}, f3,{}, f4,{}, f5,{});
+
+%% Fill return variable with data
+currDay = floor(tnum(1));
+prevDay = currDay - 1;
+nextDay = currDay;
+emptyDays = datetime([],[],[]);
+i = 1;
+for d = 1:1:nDays
+    if d == 476
+        
+    end
+    if currDay == prevDay + 1
+        % get range of data for this day
+        iFirst = i;
+        while nextDay == currDay
+            i = i+1;
+            if i > length(tnum)
+                break
+            end
+            nextDay = floor(tnum(i));
+        end
+        iLast = i-1;
+        prevDay = currDay;
+        currDay = nextDay;
+        % create struct with this day's data
+        days(d) = struct(...
+            f1, data.(f1)(iFirst:iLast), ...
+            f2, data.(f2)(iFirst:iLast), ...
+            f3, data.(f3)(iFirst:iLast), ...
+            f4, data.(f4)(iFirst:iLast), ...
+            f5, data.(f5)(iFirst:iLast));
+    else
+        prevDay = prevDay + 1;
+        emptyDays(end+1) = datetime(prevDay,'ConvertFrom','datenum');
+    end
+end
+emptyDays = emptyDays';
 
 end
