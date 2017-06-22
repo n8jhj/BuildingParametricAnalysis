575f8e668c85df0778524bb15734e5a2637889c2
Nathaniel Jones <n8jhj@live.com>
2017-06-22 14:23:24 -0700

    add buildingDaysBoxplots.m - comparative boxplots
    niceBldgs.mat was also created - a struct of the 9 of 30 buildings that
    don't require any filtering for nice boxplot results.

---
+++ MATLAB/buildingDaysBoxplots.m
@@ -0,0 +1,42 @@
+function buildingDaysBoxplots(buildings, fieldname)
+%BUILDINGBOXPLOTS Boxplots of a certain field of input buildings.
+%   buildingBoxplots(buildings, fieldname)
+%   Creates boxplots displaying field specified by FIELDNAME for each
+%   building in the set BUILDINGS, all on the same plot axes. FIELDNAME is
+%   assumed to be within building.days.
+%
+% Example:
+%   buildingBoxplots(buildings, 'tdrEn')
+%   Creates boxplots of days.tdrEn for each building in BUILDINGS.
+
+%% Create boxplot-compatible data structure
+% get maximum length of input field 'param'
+bLen = length(buildings);
+xLen = 0;
+for b = 1:1:bLen
+    xLen = xLen + length(buildings(b).days);
+end
+x = zeros(xLen,1);
+g = cell(xLen,1);
+ind = 0;
+for b = 1:1:bLen
+    field = [buildings(b).days.(fieldname)].';
+    fLen = length(field);
+    x(ind+1:ind+fLen) = field;
+    names = cell(fLen,1);
+    names(1:fLen) = cellstr(buildings(b).name);
+    g(ind+1:ind+fLen) = names.';
+    ind = ind + fLen;
+end
+
+%% Create boxplot
+close all
+boxplot(x,g,'PlotStyle','compact');
+fig1 = gcf;
+figPos = get(fig1, 'Position');
+deltaY = 1000;
+figPos(4) = figPos(4) + deltaY;
+figPos(2) = figPos(2) - deltaY;
+set(fig1, 'Position', figPos);
+
+end
