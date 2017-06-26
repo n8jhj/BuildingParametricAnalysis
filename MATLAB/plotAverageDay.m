828baf177a571d51253ee504e4416cde95b25c49
Nathaniel Jones <n8jhj@live.com>
2017-06-26 09:44:15 -0700

    add save option to plotDemandsTogether.m
    Also reformat plotAverageDay.m for consistency with
    plotDemandsTogether.m.

--- MATLAB/plotAverageDay.m
+++ MATLAB/plotAverageDay.m
@@ -1,29 +1,34 @@
 function plotAverageDay(buildings, save)
 %PLOTAVERAGEDAY Plot the average day's profiles and optionally save the
 %figure as .png file for the input buildings.
-%   plotAverageDay(buildings)
-%   BUILDINGS is a list of building structures.
-%
 %   plotAverageDay(buildings,save)
-%   SAVE is a boolean value. Default is false.
+%   BUILDINGS is the list of buildings for which the average day should be
+%   plotted. SAVE is a boolean value specifying whether the figure should
+%   be saved. Default is false.
 
-%% Handle inputs
+%% Handle input
 if nargin < 2
     save = false;
 end
 
-%% Plot and optionally save pictures for each building
+%% For each building, plot, resize, and optionally save
 fdNames = fieldnames(buildings(1).avgDay);
 fLen = length(fdNames);
 for i = 1:1:length(buildings)
-    fig = figure;
     bName = buildings(i).name;
+    
+    % Plot
+    fig = figure;
     for j = 1:1:fLen
         subplot(fLen,1,j)
         plot(buildings(i).avgDay.(fdNames{j}))
         addFigText(bName,fdNames{j})
     end
+    
+    % Resize
     resizeFigure(fig)
+    
+    % Save
     if save
         saveas(fig,strcat(bName,'_avgDay.png'))
     end
@@ -55,7 +60,9 @@ function resizeFigure(fig)
 % resize height of fig
 figPos = get(fig,'OuterPosition');
 deltaH = 700;
+deltaW = -180;
 figPos(2) = figPos(2) - deltaH;
 figPos(4) = figPos(4) + deltaH;
+figPos(3) = figPos(3) + deltaW;
 set(fig,'OuterPosition',figPos)
 end
--- MATLAB/plotDemandsTogether.m
+++ MATLAB/plotDemandsTogether.m
@@ -1,22 +1,44 @@
-function plotDemandsTogether(building, save)
+function plotDemandsTogether(buildings, save)
 %PLOTDEMANDSTOGETHER Plot of key demand graphs on one figure.
-%   plotDemandsTogether(building, save)
-%   BUILDING is the building for which data will be plotted. SAVE is an
+%   plotDemandsTogether(buildings, save)
+%   BUILDINGS is the building for which data will be plotted. SAVE is an
 %   optional boolean argument specifying whether the figure should be
 %   saved. Default is false.
 
-%% Plot
-subplot(2,2,1)
-plotDemand(building,'nofig','totFacEn');
-subplot(2,2,2)
-plotDemand(building,'nofig','totFacDe');
-subplot(2,2,3)
-plotDemandHistogram(building,'nofig','totFacEn')
-subplot(2,2,4)
-plotDemandHistogram(building,'nofig','totFacDe')
+%% Handle input
+if nargin < 2
+    save = false;
+end
 
-%% Resize
-fig = gcf;
+%% For each building, plot, resize, and optionally save
+for i = 1:1:length(buildings)
+    building = buildings(i);
+    
+    % Plot
+    fig = figure;
+    subplot(2,2,1)
+    plotDemand(building,'nofig','totFacEn');
+    subplot(2,2,2)
+    plotDemand(building,'nofig','totFacDe');
+    subplot(2,2,3)
+    plotDemandHistogram(building,'nofig','totFacEn')
+    subplot(2,2,4)
+    plotDemandHistogram(building,'nofig','totFacDe')
+
+    % Resize
+    resizeFigure(fig)
+
+    % Save
+    if save
+        saveas(fig, strcat(building.name, '_demandPlots.png'))
+    end
+end
+
+end
+
+
+%% Resize figure
+function resizeFigure(fig)
 figPos = get(fig,'OuterPosition');
 deltaW = 500;
 deltaH = 300;
@@ -24,13 +46,4 @@ figPos(2) = figPos(2) - deltaH;
 figPos(3) = figPos(3) + deltaW;
 figPos(4) = figPos(4) + deltaH;
 set(fig,'OuterPosition',figPos)
-
-%% Save
-if nargin < 2
-    save = false;
-end
-if save
-    saveas(fig, strcat(building.name, '_demandPlots.png'))
-end
-
 end
