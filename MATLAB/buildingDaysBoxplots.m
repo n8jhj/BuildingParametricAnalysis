c02f29316b432679c042d656d99c5f8fa53a125b
Nathaniel Jones <n8jhj@live.com>
2017-07-06 15:26:56 -0700

    comparative boxplots fixed
    1. Path to field specification is now allowed.
    2. Using lists instead of structs.

--- MATLAB/buildingDaysBoxplots.m
+++ MATLAB/buildingDaysBoxplots.m
@@ -3,7 +3,8 @@ function buildingDaysBoxplots(buildings, fieldname)
 %   buildingBoxplots(buildings, fieldname)
 %   Creates boxplots displaying field specified by FIELDNAME for each
 %   building in the set BUILDINGS, all on the same plot axes. FIELDNAME is
-%   assumed to be within building.days. Input BUILDINGS can be either a
+%   assumed to be within building.days and should be specified as a path
+%   given as a cell array of strings. Input BUILDINGS can be either a
 %   struct containing a set of buildings or a cell array containing
 %   multiple struct sets of buildings. In the case of multiple sets, each
 %   set is assigned a different color in the boxplot.
@@ -19,48 +20,45 @@ if ~iscell(buildings)
     buildings = {buildings};
 end
 
-%% Create one combined struct for boxplot compatibility
-% colors preset: blu, mrn, red, grn, cyn, blk, ylw
+%% Colors preset: blu, mrn, red, grn, cyn, blk, ylw
 cp = 'bmrgcky';
+% initialize colors
 colors = 'b';
-% set field names of combined struct
-f1 = 'name';
-f2 = 'data';
-f3 = 'set';
-% initialize combined struct
-combStruct = struct(...
-    f1, {buildings{1}(1).(f1)}, ...
-    f2, {buildings{1}(1).days.(fieldname)}, ...
-    f3, 1);
+
+%% Create combined lists for boxplot compatibility
+% initialize combined list
+% col 1 -> building names
+% col 2 -> building data
+lenTotal = 0;
+for set = 1:1:length(buildings)
+    for b = 1:1:length(buildings{set})
+        lenTotal = lenTotal + length(buildings{set}(b).days);
+    end
+end
+combList = cell(lenTotal, 2);
 % for each set of buildings
-bStart = 2;
-iEnd = length(buildings{1}(1).days);
+iEnd = 0;
 for set = 1:1:length(buildings)
     % add each building in the set to combined struct
-    for b = bStart:1:length(buildings{set})
+    for b = 1:1:length(buildings{set})
         iStart = iEnd + 1;
         iEnd = iStart + length(buildings{set}(b).days) - 1;
-        % set up cell arrays to assign
-        f1Cell = cell(iEnd-iStart+1, 1);
-        f2Cell = f1Cell;
-        f3Cell = f1Cell;
-        f1Cell(:) = {buildings{set}(b).(f1)};
-        f2Cell(:) = {buildings{set}(b).days.(fieldname)};
-        f3Cell(:) = {set};
-        % assign cell arrays to combined struct
-        [combStruct(iStart:iEnd).(f1)] = f1Cell{:};
-        [combStruct(iStart:iEnd).(f2)] = f2Cell{:};
-        [combStruct(iStart:iEnd).(f3)] = f3Cell{:};
+        % assign values to combined lists
+        combList(iStart:iEnd, 1) = {buildings{set}(b).name};
+        for d = 1:1:length(buildings{set}(b).days)
+            % add value to field 2 cell array
+            combList(iStart-1+d, 2) = ...
+                {getFieldByPath(buildings{set}(b), ['days',d,fieldname])};
+        end
         % update colors array
         colors(end+1) = cp(set);
     end
-    bStart = 1;
 end
 
 %% Create boxplot
 % plot
-x = [combStruct.(f2)];
-g = {combStruct.(f1)};
+x = cell2mat(combList(:,2));
+g = combList(:,1);
 fig = figure;
 boxplot(x,g, 'PlotStyle','compact', 'Colors',colors);
 % resize
