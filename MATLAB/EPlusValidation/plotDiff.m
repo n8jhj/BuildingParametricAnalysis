086935cdcf32fc5937ce0fad51b213ade7fc3ff2
Nathaniel <n8jhj@live.com>
2017-07-28 20:08:36 -0700

    plotDiff.m description and MediumOffice fig

--- MATLAB/EPlusValidation/plotDiff.m
+++ MATLAB/EPlusValidation/plotDiff.m
@@ -4,7 +4,13 @@ function plotDiff(rows,cols,figNumMin,figNumSub)
 %   Plot data from figure(FIGNUMMIN) is minuend (subtracted from); plot
 %   data from figure(FIGNUMSUB) is subtrahend (to be subtracted). FIGNUMMIN
 %   and FIGNUMSUB may each be either a single integer or a list of
-%   integers.
+%   integers. ROWS and COLS are the number of rows and columns in the
+%   active subplot figure.
+%
+% Example:
+%   plotDiff(8,2,14,[12,10,8])
+%   Plots the difference between subplots 14 and the sum of subplots 12,
+%   10, and 8 from a subplot figure with 8 rows and 2 columns.
 
 %% Get plots data
 [x,y] = comparisonPlots(rows,cols);
